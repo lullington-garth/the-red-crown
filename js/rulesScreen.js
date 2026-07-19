d0de3a1b71c827c99ae594e21876e08d7b65f7d5
lullington-garth <livepileplus@gmail.com>
2026-07-19 19:09:28 +0100

    Update rulesScreen.js

--- js/rulesScreen.js
+++ js/rulesScreen.js
@@ -23,7 +23,6 @@ export function showRulesScreen(container, onContinue) {
             gameDiv.style.backgroundImage = "url('./images/paper.jpg')";
             gameDiv.style.backgroundSize = "cover";
             gameDiv.style.backgroundPosition = "center";
-    //        gameDiv.style.backgroundColor = "white";
             document.body.style.backgroundColor = "black";
 
     }
@@ -36,6 +35,7 @@ export function showRulesScreen(container, onContinue) {
     wrapper.style.margin = "0 auto";
     wrapper.style.fontFamily = "'Book Antiqua', Palatino, serif";
     wrapper.style.lineHeight = "1.6";
+    wrapper.style.backgroundColor = "#f8f3f306";
 //    wrapper.style.backgroundImage = "inherit";
     // ------------------------------------------------
     // Helpers
@@ -147,13 +147,11 @@ export function showRulesScreen(container, onContinue) {
 
     const img = document.createElement("img");
     img.src = `mapImages/crown.jpg`;
-    img.style.backgroundImage = "url('./images/paper.jpg')";
     img.style.width = "100%";
     img.style.maxWidth = "325px";
     img.style.display = "block";
     img.style.margin = "0 auto 10px";
     img.style.mixBlendMode = "multiply";
-    img.style.isolation = "isolate";
     wrapper.appendChild(img);
  
     heading("THE RED CROWN");
