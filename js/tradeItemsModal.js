6146e73d8313dea8f656455240f6bc43624907fb
lullington-garth <livepileplus@gmail.com>
2026-08-09 14:22:34 +0100

    Update tradeItemsModal.js

--- js/tradeItemsModal.js
+++ js/tradeItemsModal.js
@@ -127,6 +127,7 @@ export function openTradeItemsModal(playerStats, {
             row.style.borderBottom = "1px solid #444";
             row.style.cursor = "pointer";
             row.style.borderRadius = "6px";
+            row.style.backgroundImage = "url('./images/paper.jpg')";
 
             const display = showItemDescription(
                 item,
