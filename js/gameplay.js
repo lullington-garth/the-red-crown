f6cdd2040ff95197f36583441e7efac54d32e1c4
lullington-garth <livepileplus@gmail.com>
2026-08-11 17:43:16 +0100

    Worm of Sligoff

--- js/gameplay.js
+++ js/gameplay.js
@@ -107,7 +107,7 @@ export function startGameplay(
 
     // track current node
 
-    engine.start(765);
+    engine.start(775);
 
 function returnToMap() {
     engine.goToNode(engine.state.currentNode);
