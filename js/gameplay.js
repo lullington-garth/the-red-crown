ddc7daf0a7c211e822c8b0c9195576e009d9c8e4
lullington-garth <livepileplus@gmail.com>
2026-08-11 21:15:25 +0100

    Mineshaft

--- js/gameplay.js
+++ js/gameplay.js
@@ -107,7 +107,7 @@ export function startGameplay(
 
     // track current node
 
-    engine.start(785);
+    engine.start(730);
 
 function returnToMap() {
     engine.goToNode(engine.state.currentNode);
