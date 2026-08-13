1b5e58b4ae1517a444fc096b639c9970f89bf027
lullington-garth <livepileplus@gmail.com>
2026-08-13 14:11:22 +0100

    Update gameplay.js

--- js/gameplay.js
+++ js/gameplay.js
@@ -107,7 +107,7 @@ export function startGameplay(
 
     // track current node
 
-    engine.start(868);
+    engine.start(865);
 
 function returnToMap() {
     engine.goToNode(engine.state.currentNode);
