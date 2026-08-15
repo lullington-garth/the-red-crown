2b5e4afcd4c08452430b23d28b6a51475825746d
lullington-garth <livepileplus@gmail.com>
2026-08-15 14:12:36 +0100

    Graffle Cluck Game

--- js/gameplay.js
+++ js/gameplay.js
@@ -106,7 +106,7 @@ export function startGameplay(
 
     // track current node
 
-    engine.start(585);
+    engine.start(612);
 
 function returnToMap() {
     engine.goToNode(engine.state.currentNode);
