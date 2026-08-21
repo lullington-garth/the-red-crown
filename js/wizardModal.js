118bbff288b0266caf95fe77d4edaa7f1c638cc5
lullington-garth <livepileplus@gmail.com>
2026-08-21 21:27:29 +0100

    Update wizardModal.js

--- js/wizardModal.js
+++ js/wizardModal.js
@@ -100,4 +100,11 @@ export function showWizardModal(playerStats) {
     hideModalCloseButton();
     showModalCloseButton();
     openModal();
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "instant"
+    });
+
 }
